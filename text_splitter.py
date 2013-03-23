0c06d9feab791aba583836ffdbd1e51430160a5b
Philip Cote <cotejrp@cotejrp-NP200AA-ABA-a6720y.(none)>
2013-03-22 20:50:33 -0400

    Basic panel button interface in the 3D tools margin added.

--- text_splitter.py
+++ text_splitter.py
@@ -1,8 +1,5 @@
 import bpy
 
-def main(context):
-    pass    
-
 
 class TextSplitOperator(bpy.types.Operator):
     """Text split operator"""
@@ -16,27 +13,42 @@ class TextSplitOperator(bpy.types.Operator):
         return is_valid
 
     def execute(self, context):
-        #main(context)
         scn = context.scene
-        txt_ob = context.active_object.data
-        txt_str = txt_ob.body
+        txt_ob = context.active_object
+        txt_data = context.active_object.data
+        txt_str = txt_data.body
         txt_lst = txt_str.split()
-        
+
         for the_word in txt_lst:
             txt_curve = bpy.data.curves.new(the_word, type="FONT")
             txt_curve.body = the_word
             new_ob = bpy.data.objects.new(the_word, txt_curve)
             scn.objects.link(new_ob)
-                
+            new_ob.location = txt_ob.location
+
         return {'FINISHED'}
+
+
+class TextSplitPanel(bpy.types.Panel):
+    """Text Splitting Tool"""
+    bl_label = "Text Splitter"
+    bl_idname = "SCENE_PT_splittext"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'TOOLS'
+
+    def draw(self, context):
+        layout = self.layout
+        layout.operator(TextSplitOperator.bl_idname)
 
 
 def register():
     bpy.utils.register_class(TextSplitOperator)
+    bpy.utils.register_class(TextSplitPanel)
 
 
 def unregister():
     bpy.utils.unregister_class(TextSplitOperator)
+    bpy.utils.unregister_class(TextSplitPanel)
 
 
 if __name__ == "__main__":
