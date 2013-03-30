31129b5d006c7505ea16ef1cc9b45d0ee81a1e29
Philip Cote <cotejrp@cotejrp-NP200AA-ABA-a6720y.(none)>
2013-03-30 09:54:32 -0400

    Initial offset feature for text word spacing established.

--- text_splitter.py
+++ text_splitter.py
@@ -1,15 +1,51 @@
+# ***** BEGIN GPL LICENSE BLOCK *****
+#
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# ***** END GPL LICENCE BLOCK *****
+
+# (c) 2011 Phil Cote (cotejrp1)
 import bpy
+from bpy_extras.object_utils import AddObjectHelper, object_data_add
 
+bl_info = {
+    'name': 'Text Splitter',
+    'author': 'Phil Cote, cotejrp1',
+    'version': (0,1),
+    "blender": (2,66,3),
+    "location": 'View3D > ',
+    "description": "Splits up text objects into multiple words",
+    "warning": "",
+    "category": "Text"
+}
 
-class TextSplitOperator(bpy.types.Operator):
+class TextSplitOperator(bpy.types.Operator, AddObjectHelper):
     """Text split operator"""
     bl_idname = "text.split"
     bl_label = "Text Splitter"
+    bl_options = {'REGISTER', 'UNDO', 'PRESET'}
+    
+    text_offset = bpy.props.FloatProperty(name="Text Offset",
+                                        description="Offset of the text",
+                                        min=0, max=10, default=0)
 
     @classmethod
     def poll(cls, context):
         ob = context.active_object
-        is_valid = ob is not None and ob.type == 'FONT'
+        is_valid = ob != None and ob.type == 'FONT'
         return is_valid
 
     def execute(self, context):
@@ -18,18 +54,23 @@ class TextSplitOperator(bpy.types.Operator):
         txt_data = context.active_object.data
         txt_str = txt_data.body
         txt_lst = txt_str.split()
-
+        
+        cur_offset = 0
+        
         for the_word in txt_lst:
             txt_curve = bpy.data.curves.new(the_word, type="FONT")
             txt_curve.body = the_word
             new_ob = bpy.data.objects.new(the_word, txt_curve)
             scn.objects.link(new_ob)
             new_ob.location = txt_ob.location
+            new_ob.location.x += cur_offset
+            cur_offset += self.text_offset
 
         return {'FINISHED'}
 
 
 class TextSplitPanel(bpy.types.Panel):
+    
     """Text Splitting Tool"""
     bl_label = "Text Splitter"
     bl_idname = "SCENE_PT_splittext"
@@ -38,7 +79,7 @@ class TextSplitPanel(bpy.types.Panel):
 
     def draw(self, context):
         layout = self.layout
-        layout.operator(TextSplitOperator.bl_idname)
+        layout.row().operator(TextSplitOperator.bl_idname)
 
 
 def register():
